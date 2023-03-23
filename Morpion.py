cf8845df4d3d5df87f6350930cc432cf53913525
Nacer Eddine Attar <nacer.attar@laplateforme.io>
2023-03-23 21:41:57 +0100

    début d'ia avec morpion a bouton

--- Morpion.py
+++ Morpion.py
@@ -1,132 +1,214 @@
-##-----Importation des Modules-----##
-from tkinter import *
+from tkinter import*
+from tkinter import messagebox
 
-##----- Définition des Variables globales -----##
-cases=[ [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
-drapeau = True                              # True pour les croix, False pour les ronds
-n = 1                                       # Numéro du tour de jeu
+root = Tk()
+root.title('Tic Tac Toe')
+
+#X starts so true
+clicked = True
+count = 0
+
+#disable all buttons
+
+def disable_all_buttons():
+    b1.config(state=DISABLED)
+    b2.config(state=DISABLED)
+    b3.config(state=DISABLED)
+
+    b4.config(state=DISABLED)
+    b5.config(state=DISABLED)
+    b6.config(state=DISABLED)
+
+    b7.config(state=DISABLED)
+    b8.config(state=DISABLED)
+    b9.config(state=DISABLED)
+
+#check to see if someone won
+#check to see if someone won
+def checkwin():
+    global winner
+    winner = False
+#check X win
+    if b1["text"] == 'X' and b2["text"] == 'X' and b3["text"] == 'X':
+        b1.config(bg="red")
+        b2.config(bg="red")
+        b3.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Gon give it to you !")
+        disable_all_buttons()
+    elif b4["text"] == 'X' and b5["text"] == 'X' and b6["text"] == 'X':
+        b4.config(bg="red")
+        b5.config(bg="red")
+        b6.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Gon give it to you !")
+        disable_all_buttons()
+    elif b7["text"] == 'X' and b8["text"] == 'X' and b9["text"] == 'X':
+        b7.config(bg="red")
+        b8.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Gon give it to you !")
+        disable_all_buttons()
+    elif b1["text"] == 'X' and b4["text"] == 'X' and b7["text"] == 'X':
+        b1.config(bg="red")
+        b4.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Gon give it to you !")
+        disable_all_buttons()
+    elif b2["text"] == 'X' and b5["text"] == 'X' and b8["text"] == 'X':
+        b2.config(bg="red")
+        b5.config(bg="red")
+        b8.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Gon give it to you !")
+        disable_all_buttons()
+    elif b3["text"] == 'X' and b6["text"] == 'X' and b9["text"] == 'X':
+        b3.config(bg="red")
+        b6.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Gon give it to you !")
+        disable_all_buttons()
+    elif b1["text"] == 'X' and b5["text"] == 'X' and b9["text"] == 'X':
+        b1.config(bg="red")
+        b5.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Gon give it to you !")
+        disable_all_buttons()
+    elif b3["text"] == 'X' and b5["text"] == 'X' and b7["text"] == 'X':
+        b3.config(bg="red")
+        b5.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Gon give it to you !")
+        disable_all_buttons()
+#check O win
+    elif b1["text"] == 'O' and b2["text"] == 'O' and b3["text"] == 'O':
+        b1.config(bg="red")
+        b2.config(bg="red")
+        b3.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Don't mess with my O's !")
+        disable_all_buttons()
+    elif b4["text"] == 'O' and b5["text"] == 'O' and b6["text"] == 'O':
+        b4.config(bg="red")
+        b5.config(bg="red")
+        b6.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Don't mess with my O's !")
+        disable_all_buttons()
+    elif b7["text"] == 'O' and b8["text"] == 'O' and b9["text"] == 'O':
+        b7.config(bg="red")
+        b8.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Don't mess with my O's !")
+        disable_all_buttons()
+    elif b1["text"] == 'O' and b4["text"] == 'O' and b7["text"] == 'O':
+        b1.config(bg="red")
+        b4.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Don't mess with my O's !")
+        disable_all_buttons()
+    elif b2["text"] == 'O' and b5["text"] == 'O' and b8["text"] == 'O':
+        b2.config(bg="red")
+        b5.config(bg="red")
+        b8.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Don't mess with my O's !")
+        disable_all_buttons()
+    elif b3["text"] == 'O' and b6["text"] == 'O' and b9["text"] == 'O':
+        b3.config(bg="red")
+        b6.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Don't mess with my O's !")
+        disable_all_buttons()
+    elif b1["text"] == 'O' and b5["text"] == 'O' and b9["text"] == 'O':
+        b1.config(bg="red")
+        b5.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Don't mess with my O's !")
+        disable_all_buttons()
+    elif b3["text"] == 'O' and b5["text"] == 'O' and b7["text"] == 'O':
+        b3.config(bg="red")
+        b5.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "O Gon give it to you !")
+        disable_all_buttons()
+    
+    #check tie
+    if count == 9 and winner == False:
+        messagebox.showinfo("Tic Tac Toe", "Overtime ? I think ...")
 
 
-##----- Définition des Fonctions -----##
-def afficher(event) :
-    """ Entrées : Un événement de la souris
-        Sortie : Affiche en temps réel les coordonnées de la case du clic de souris"""
-    global drapeau, cases, n
-    l = (event.y-2)//100                    # Ligne du clic
-    c = (event.x-2)//100                    # Colonne du clic
-
-    if (n < 10) and (cases[l][c] == 0):
-        if drapeau:                              # drapeau == True
-            dessin.create_line(100*c+8, 100*l+8, 100*c+96, 100*l+96, width = 5, fill = 'blue')
-            dessin.create_line(100*c+8, 100*l+96, 100*c+96, 100*l+8, width = 5, fill = 'blue')
-            cases[l][c] = 1
-            message.configure(text='Aux ronds de jouer')
 
 
-        else:
-            dessin.create_oval(100*c+8, 100*l+8, 100*c+96, 100*l+96, width = 5, outline = 'red')
-            cases[l][c] = -1
-            message.configure(text='Aux croix de jouer')
+#button clicked function
 
-        drapeau = not(drapeau)
-        if (n >= 5) and (n <= 9):
-            somme = verif(cases)
-            if somme == 1 or somme == -1:
-                n = gagner(somme)
-            elif n == 9:
-                n = gagner(0)
-        n += 1
+def b_click(b):
+    global clicked, count
 
+    if b["text"] == " " and clicked == True:
+        b["text"] = "X"
+        clicked = False
+        count += 1
+        checkwin()
+    elif  b["text"] == " " and clicked == False:
+        b["text"] = "O"
+        clicked = True
+        count += 1
+        checkwin()
+    else:
+        messagebox.showerror("Tic Tac Toe", "You blind ?\nClick somewhere else !")
 
-def verif(tableau):
-    """ Entrées : un tableau "carré"
-        Sorties : Calcule les sommes de chaque ligne/colonne/diagonale
-            et vérifie l'alignement."""
-    sommes = [0,0,0,0,0,0,0,0]             # Il y a 8 sommes à vérifier
-    # Les lignes :
-    sommes[0] = sum(tableau[0])
-    sommes[1] = sum(tableau[1])
-    sommes[2] = sum(tableau[2])
-    # Les colonnes
-    sommes[3] = tableau[0][0]+tableau[1][0]+tableau[2][0]
-    sommes[4] = tableau[0][1]+tableau[1][1]+tableau[2][1]
-    sommes[5] = tableau[0][2]+tableau[1][2]+tableau[2][2]
-    # Les diagonales
-    sommes[6] = tableau[0][0]+tableau[1][1]+tableau[2][2]
-    sommes[7] = tableau[0][2]+tableau[1][1]+tableau[2][0]
+def reset():
+    global b1,b2,b3,b4,b5,b6,b7,b8,b9
+    global clicked, count
+    clicked = True
+    count = 0
 
-    for i in range(8):                     # Parcours des sommes
-        if sommes[i] == 3:
-            return 1
-        elif sommes[i] == -3:
-            return -1
-    return 0
+    #Build our buttons
+    b1 = Button(root, text=" ", font=('helvetica', 20), height=3, width=6, bg="SystemButtonFace", command=lambda : b_click (b1))
+    b2 = Button(root, text=" ", font=('helvetica', 20), height=3, width=6, bg="SystemButtonFace", command=lambda : b_click (b2))
+    b3 = Button(root, text=" ", font=('helvetica', 20), height=3, width=6, bg="SystemButtonFace", command=lambda : b_click (b3))
 
+    b4 = Button(root, text=" ", font=('helvetica', 20), height=3, width=6, bg="SystemButtonFace", command=lambda : b_click (b4))
+    b5 = Button(root, text=" ", font=('helvetica', 20), height=3, width=6, bg="SystemButtonFace", command=lambda : b_click (b5))
+    b6 = Button(root, text=" ", font=('helvetica', 20), height=3, width=6, bg="SystemButtonFace", command=lambda : b_click (b6))
 
+    b7 = Button(root, text=" ", font=('helvetica', 20), height=3, width=6, bg="SystemButtonFace", command=lambda : b_click (b7))
+    b8 = Button(root, text=" ", font=('helvetica', 20), height=3, width=6, bg="SystemButtonFace", command=lambda : b_click (b8))
+    b9 = Button(root, text=" ", font=('helvetica', 20), height=3, width=6, bg="SystemButtonFace", command=lambda : b_click (b9))
 
-def gagner(a):
-    """Cette fonction indique le gagnant en mo difiant le message et en
-        renvoyant la valeur 9."""
-    if a == 1:
-        message.configure(text = 'Les croix ont gagné !')
-    elif a == -1:
-        message.configure(text = 'Les ronds ont gagné !')
-    elif a == 0:
-        message.configure(text = 'Match nul !')
-    return 9
+    #grid button to the screen
+    b1.grid (row=0,column=0)
+    b2.grid (row=0,column=1)
+    b3.grid (row=0,column=2)
 
+    b4.grid (row=1,column=0)
+    b5.grid (row=1,column=1)
+    b6.grid (row=1,column=2)
 
+    b7.grid (row=2,column=0)
+    b8.grid (row=2,column=1)
+    b9.grid (row=2,column=2)
 
-def reinit():
-    """Cette fonction ré-initialise les variables globales."""
-    global drapeau, cases, n
-    cases = [[0, 0, 0],
-             [0, 0, 0],
-             [0, 0, 0]]
-    drapeau = True          # True pour les croix, False pour les ronds
-    n = 1
+#create menu
+my_menu=Menu(root)
+root.config(menu=my_menu)
 
-    message.configure(text='Aux croix de jouer')
-    dessin.delete(ALL)      # Efface toutes les figures
-    lignes = []
-    for i in range(4):
-      lignes.append(dessin.create_line(0, 100*i+2, 303, 100*i+2, width=3))
-      lignes.append(dessin.create_line(100*i+2, 0, 100*i+2, 303, width=3))
+#create options menu
+options_menu=Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="Options", menu=options_menu )
+options_menu.add_command(label="Reset Game", command=reset)
 
+reset()
 
-##-----Création de la fenêtre-----##
-fen = Tk()
-fen.title('Morpion')
-
-
-##-----Création des zones de texte-----##
-message=Label(fen, text='Aux croix de jouer')
-message.grid(row = 0, column = 0, columnspan=2, padx=3, pady=3, sticky = W+E)
-
-
-##-----Création des boutons-----##
-bouton_quitter = Button(fen, text='Quitter', command=fen.destroy)
-bouton_quitter.grid(row = 2, column = 1, padx=3, pady=3, sticky = S+W+E)
-
-bouton_reload = Button(fen, text='Recommencer', command=reinit)
-bouton_reload.grid(row = 2, column = 0, padx=3, pady=3, sticky = S+W+E)
-
-
-##-----Création du canevas-----##
-dessin=Canvas(fen, bg="white", width=301, height=301)
-dessin.grid(row = 1, column = 0, columnspan = 2, padx=5, pady=5)
-
-
-##-----La grille-----##
-lignes = []
-
-
-##-----Evenements-----##
-dessin.bind('<Button-1>', afficher)
-
-
-##-----Programme principal-----##
-reinit()
-fen.mainloop()                 # Boucle d'attente des événements
+root.mainloop()
--- ia_qui_te_demarre.py
+++ ia_qui_te_demarre.py
@@ -1,12 +1,1 @@
-import random
-from Morpion import cases
-def ia_random():
-    empty_cells = []
-    for l in range(3):
-        for c in range(3):
-            if cases[l][c] == 0:
-                    empty_cells.append((l, c))
-    if empty_cells:
-        return random.choice(empty_cells)
-    else:
-        return False
+from morpionwthcodemy import b1,
